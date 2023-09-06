db680834c35c236660f1539454a404a98ea07ae8
Drew Jordan <ajordan12@luc.edu>
2023-09-06 10:48:25 -0500

    chapter2A lab 1 files

---
+++ Week2/Chapter2A_Lab1/Oops2.java
@@ -0,0 +1,14 @@
+public class Oops2 {
+    public static void main(String[] args){
+        int x = 0;
+        System.out.println("x is " + x);
+        part2();
+    }
+    public static void part2(){
+        int x = 15;
+        System.out.println("x is now " + x);
+
+        int y = x + 1;
+        System.out.println("x and y are " + x + " and " + y);
+    }
+}
---
+++ Week2/Chapter2A_Lab1/expressions1.java
@@ -0,0 +1,44 @@
+public class expressions1 {
+    public static void main(String[] args){
+        int math1 = 2 + 3 * 4 - 6;
+        System.out.println(math1);
+        int math2 = 14 / 7 * 2 + 30 / 5 + 1;
+        System.out.println(math2);
+        int math3 =  (12 + 3) / 4 * 2;
+        System.out.println(math3);
+        int math4 =  (238 % 10 + 3) % 7;
+        System.out.println(math4);
+        int math5 = (18 - 7) * (43 % 10);
+        System.out.println(math5);
+        int math6 = 2 + 19 % 5 - (11 * (5 / 2));
+        System.out.println(math6);
+        double math7 = 813 % 100 / 3 + 2.4;
+        System.out.println(math7);
+        int math8 = 26 % 10 % 4 * 3;
+        System.out.println(math8);
+        int math9 = 22 + 4 * 2;
+        System.out.println(math9);
+        int math10 = 23 % 8 % 3;
+        System.out.println(math10);
+        int math11 = 12 - 2 - 3;
+        System.out.println(math11);
+        int math12 = 6/2 + 7/3;
+        System.out.println(math12);
+        int math13 = 6 * 7 % 4;
+        System.out.println(math13);
+        int math14 = 3 * 4 + 2 * 3;
+        System.out.println(math14);
+        int math15 = 177 % 100 % 10 / 2;
+        System.out.println(math15);
+        int math16 = 89 % (5 + 5) % 5;
+        System.out.println(math16);
+        int math18 = 392 / 10 % 10 / 2;
+        System.out.println(math18);
+        int math19 = 8 * 2 - 7 / 4;
+        System.out.println(math19);
+        int math20 = 37 % 20 % 3 * 4;
+        System.out.println(math20);
+        int math21 = 17 % 10 / 4;
+        System.out.println(math21);
+    }
+}
