3718d95cc914ae625f02d5e366d6e871d28f3d96
Drew Jordan <ajordan12@luc.edu>
2023-09-10 13:13:53 -0500

    mississippi hw

---
+++ MISSISSIPPI.java
@@ -0,0 +1,46 @@
+public class MISSISSIPPI {
+    public static void main(String[] args){
+        M(); I(); S();  S(); I(); S(); S(); I(); P(); P(); I();
+    }
+    public static void M(){
+        System.out.println("M     M");
+        System.out.println("MM   MM");
+        System.out.println("M M M M");
+        System.out.println("M  M  M");
+        System.out.println("M     M");
+        System.out.println("M     M");
+        System.out.println("M     M");
+        // System.out.println();
+    }
+    public static void I(){
+        System.out.println("IIIII");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("  I  ");
+        System.out.println("IIIII");
+        System.out.println();
+    }
+    public static void S(){
+        System.out.println(" SSSSS ");
+        System.out.println("S     S");
+        System.out.println("S      ");
+        System.out.println(" SSSSS ");
+        System.out.println("      S");
+        System.out.println("S     S");
+        System.out.println(" SSSSS ");
+        // System.out.println();
+    }
+    public static void P(){
+        System.out.println("PPPPPP ");
+        System.out.println("P     P");
+        System.out.println("P     P");
+        System.out.println("PPPPPP ");
+        System.out.println("P      ");
+        System.out.println("P      ");
+        System.out.println("P      ");
+        // System.out.println();
+    }
+
+}
