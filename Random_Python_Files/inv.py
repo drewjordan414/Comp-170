f9ea00e469f3bc3eee5978ed504b07341d8e81b3
Drew Jordan <ajordan12@luc.edu>
2023-09-23 12:32:36 -0500

    updates to inverse matrix

--- Random_Python_Files/inv.py
+++ Random_Python_Files/inv.py
@@ -1,21 +1,22 @@
+# print(augmented_matrix)
 import numpy as np
 
-# array
+# Given array
 A = np.array([[-6,-5,5],
               [7, -2, -1],
               [9,8,7]])
-# take the inverse of the array using numpy
-# B = np.linalg.inv(A)
-# print(B)
 
-
-
-
-# take the inverse of the array using elementary row operations
 n = A.shape[0]
 augmented_matrix = np.hstack((A, np.eye(n)))
 
 for i in range(n):
+    # Check if the diagonal is zero, if yes, pivot
+    if augmented_matrix[i, i] == 0:
+        for k in range(i+1, n):
+            if augmented_matrix[k, i] != 0:
+                augmented_matrix[[i, k]] = augmented_matrix[[k, i]]  # Swap rows
+                break
+    
     factor = augmented_matrix[i, i]
     augmented_matrix[i] /= factor
     
@@ -23,6 +24,19 @@ for i in range(n):
         if i != j:
             factor = augmented_matrix[j, i]
             augmented_matrix[j] -= factor * augmented_matrix[i]
-    
-print(augmented_matrix)
+
+# Extracting the inverse matrix from the augmented matrix
+A_inverse_computed = augmented_matrix[:, n:]
+
+# Multiply the original matrix with the computed inverse
+product = np.dot(A, A_inverse_computed)
+
+# Check if the product is close to an identity matrix
+is_identity = np.allclose(product, np.eye(n))
+print("Is the computed matrix the correct inverse?", is_identity)
+
+
+
+
+
 
